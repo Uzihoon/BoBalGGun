0e0dbe2ddab3fa991208ffd098e79f98a8e07c14
jiwoohong <uzihoon.dev@gmail.com>
2020-04-16 22:38:39 +0900

    set analysis component

--- src/components/Analysis/Analysis.tsx
+++ src/components/Analysis/Analysis.tsx
@@ -1,14 +1,79 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import {TouchableOpacity, ScrollView, View, Text, Image} from 'react-native';
+import Arrow from '../../assets/arrow.png';
 import styles from './styles';
+import Good from '../../assets/good.png';
+
 function Analysis() {
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text>Hello</Text>
-        <Text>BYE</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={styles.wrapper}>
+      <View style={styles.confirmBox}>
+        <View style={styles.line}>
+          <View style={{...styles.betweenStation, marginRight: 55}}>
+            <Image source={Arrow} style={styles.icon} />
+            <Text style={styles.betweenText}>서울대입구</Text>
+          </View>
+          <View style={{...styles.betweenStation, marginLeft: 55}}>
+            <Image
+              source={Arrow}
+              style={{...styles.icon, transform: [{rotateY: '180deg'}]}}
+            />
+            <Text style={styles.betweenText}>사당</Text>
+          </View>
+        </View>
+        <View style={styles.infoBox}>
+          <View style={styles.info}>
+            <View style={styles.infoText}>
+              <Text style={styles.lineInfo}>2호선</Text>
+              <Text style={styles.stationInfo}>낙성대</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.comingBox}>
+        <Text style={styles.coming}>
+          열차가 <Text style={styles.point}>15:00</Text> 분 후 도착할 예정
+          입니다.
+        </Text>
+      </View>
+      <View style={styles.analysis}>
+        <Text style={styles.title}>낙성대역에서 사당 방면으로 앉아갈 확률</Text>
+        <View style={styles.faceBox}>
+          <Image style={styles.face} source={Good} />
+        </View>
+        <Text style={styles.percent}>52%</Text>
+        <Text style={styles.comment}>아마도.. 앉아서 갈수 있을꺼에요</Text>
+      </View>
+      <View style={styles.analysis}>
+        <View style={styles.container}>
+          <View style={styles.statisticsBox}>
+            <Text style={styles.label}>하차를 많이 하는 칸</Text>
+            <Text style={styles.value}>6-2</Text>
+          </View>
+          <View style={styles.statisticsBox}>
+            <Text style={styles.label}>승객이 가장 적은 칸</Text>
+            <Text style={styles.value}>5호칸</Text>
+          </View>
+          <View style={styles.statisticsBox}>
+            <Text style={styles.label}>승객이 가장 적은 시간</Text>
+            <Text style={styles.value}>09:30</Text>
+          </View>
+          <View style={styles.statisticsBox}>
+            <Text style={styles.label}>평균 혼잡도</Text>
+            <Text style={styles.value}>혼잡</Text>
+          </View>
+        </View>
+      </View>
+      <Text style={styles.desc}>
+        현재 시각에 따른 기준입니다. 날씨나 특정 이벤트 등에 따라 다를 수
+        있습니다.
+      </Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>다른 역 알아보기</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
--- src/components/Analysis/styles.ts
+++ src/components/Analysis/styles.ts
@@ -1,5 +1,167 @@
 import {StyleSheet} from 'react-native';
+import {Color} from '../../styles/common';
 
-const styles = StyleSheet.create({});
+const todo = '#3cb44a';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#f2f3f9',
+  },
+  confirmBox: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  line: {
+    backgroundColor: todo,
+    height: 119,
+    alignSelf: 'stretch',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  infoBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 33,
+  },
+  betweenStation: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 119,
+  },
+  betweenText: {
+    fontSize: 18,
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  icon: {
+    width: 32,
+    height: 10,
+    marginBottom: 8,
+  },
+  info: {
+    width: 136,
+    height: 136,
+    borderRadius: 136 / 2,
+    borderWidth: 9.7,
+    borderColor: todo,
+    backgroundColor: '#ffffff',
+    textAlign: 'center',
+  },
+  infoText: {
+    justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  lineInfo: {
+    fontSize: 18,
+    color: todo,
+    fontWeight: 'bold',
+  },
+  stationInfo: {
+    marginTop: 5,
+    fontSize: 23,
+    color: todo,
+    fontWeight: 'bold',
+  },
+  comingBox: {
+    marginTop: 70,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  point: {
+    color: todo,
+  },
+  coming: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 17,
+    color: 'rgb(68,68,68)',
+  },
+  analysis: {
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    padding: 22,
+    shadowColor: 'rgba(0, 0, 0, 0.35)',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+    margin: 22,
+    marginBottom: 0,
+  },
+  title: {
+    textAlign: 'center',
+    color: 'rgb(180,180,180)',
+    fontSize: 16,
+  },
+  faceBox: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 19,
+    marginBottom: 19,
+  },
+  face: {
+    width: 128,
+    height: 128,
+  },
+  percent: {
+    fontSize: 62,
+    fontWeight: 'bold',
+    color: 'rgb(66,66,66)',
+    textAlign: 'center',
+  },
+  comment: {
+    marginTop: 17,
+    textAlign: 'center',
+    fontSize: 18,
+    color: 'rgb(180,180,180)',
+  },
+  desc: {
+    marginLeft: 22,
+    marginTop: 14,
+    fontSize: 10,
+    color: 'rgb(192,192,192)',
+  },
+  statisticsBox: {
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  container: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  label: {
+    fontSize: 15,
+    color: 'rgb(180,180,180)',
+    marginBottom: 18,
+  },
+  value: {
+    fontSize: 37,
+    color: todo,
+    fontWeight: 'bold',
+  },
+  button: {
+    marginTop: 48,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: Color.theme,
+    fontWeight: 'bold',
+  },
+});
 
 export default styles;
