0f9d61708f67649802e4dc2ec1f142a1e6968d44
jiwoohong <uzihoon.dev@gmail.com>
2020-04-18 14:17:53 +0900

    set permission

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {request, check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import Permission from './Permission';
 import Confirmation from './Confirmation';
 import Analysis from './Analysis';
@@ -6,6 +7,35 @@ import Search from './Search';
 import Empty from './Empty';
 
 function App() {
-  return <Empty />;
+  useEffect(() => {
+    request(PERMISSIONS.IOS.LOCATION_ALWAYS).then((result) => {
+      console.log(result);
+    });
+    check(PERMISSIONS.IOS.LOCATION_ALWAYS)
+      .then((result) => {
+        switch (result) {
+          case RESULTS.UNAVAILABLE:
+            console.log(
+              'This feature is not available (on this device / in this context)',
+            );
+            break;
+          case RESULTS.DENIED:
+            console.log(
+              'The permission has not been requested / is denied but requestable',
+            );
+            break;
+          case RESULTS.GRANTED:
+            console.log('The permission is granted');
+            break;
+          case RESULTS.BLOCKED:
+            console.log('The permission is denied and not requestable anymore');
+            break;
+        }
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }, []);
+  return <Analysis />;
 }
 export default App;
